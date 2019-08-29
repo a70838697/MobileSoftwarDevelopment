191eb3cdbc25e7999c2f38e97aea851040c97dcc
casper chen <70838697@qq.com>
2019-08-29 20:55:36 +0800

    通过findViewById获得TextView控件，调用其setText方法设置其内容

--- app/src/main/java/com/example/casper/MainActivity.java
+++ app/src/main/java/com/example/casper/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.casper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textViewHelloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewHelloWorld=findViewById(R.id.textViewHelloWorld);
+        textViewHelloWorld.setText("你好，编程实现的Hello world!");
     }
 }
