a08a8b82028c379e285d7639f50dbed3adf01556
casper chen <70838697@qq.com>
2019-09-01 16:58:05 +0800

    TextView测试已添加

--- app/src/androidTest/java/com/example/casper/TextViewMainActivityTest.java
+++ app/src/androidTest/java/com/example/casper/TextViewMainActivityTest.java
@@ -4,10 +4,16 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,12 +22,20 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class TextViewMainActivityTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.casper", appContext.getPackageName());
+    }
+    @Rule
+    public ActivityTestRule<TextViewMainActivity> activityRule =
+            new ActivityTestRule<>(TextViewMainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello World")).check(matches(isDisplayed()));
     }
 }
