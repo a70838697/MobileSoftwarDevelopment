c131996a346fe7a6f2f84d088b659c1bb890afe4
casper chen <70838697@qq.com>
2019-09-14 13:52:36 +0800

    bug free project

--- app/src/main/java/com/example/casper/ImageViewMainActivity.java
+++ app/src/main/java/com/example/casper/ImageViewMainActivity.java
@@ -9,22 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ImageViewMainActivity extends AppCompatActivity {
 
-    private Button buttonPrevious = findViewById(R.id.button_previous), buttonNext = findViewById(R.id.buttonHelloEn);
-    private ImageView imageViewFunny = findViewById(R.id.image_view_funny);
+    private Button buttonPrevious, buttonNext;
+    private ImageView imageViewFunny;
     private int[] imageIDArray = {R.drawable.funny_1, R.drawable.funny_2
             , R.drawable.funny_3, R.drawable.funny_4, R.drawable.funny_5
             , R.drawable.funny_6
     };
-    private int imageIDArrayCurrentIndex;
-
-    public ImageViewMainActivity(int iTest) {
-        imageIDArrayCurrentIndex = 0;
-    }
-
+    private int imageIDArrayCurrentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view_main);
+
+        buttonPrevious = findViewById(R.id.button_previous);
+        buttonNext = findViewById(R.id.button_next);
+        imageViewFunny = findViewById(R.id.image_view_funny);
 
         buttonPrevious.setOnClickListener(new MyButtonClickListener());
         buttonNext.setOnClickListener(new MyButtonClickListener());
@@ -34,7 +33,7 @@ public class ImageViewMainActivity extends AppCompatActivity {
     private class MyButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            if (((Button) view).getText() == "下一个") {
+            if (view == buttonNext) {
                 imageIDArrayCurrentIndex = (imageIDArrayCurrentIndex + 1) % imageIDArray.length;
             } else {
                 imageIDArrayCurrentIndex = (imageIDArrayCurrentIndex + imageIDArray.length - 1) % imageIDArray.length;
